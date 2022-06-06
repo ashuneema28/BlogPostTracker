a6d07d41e5bf138b6bf3e9b32023fb7f298c43ba
ashuneema28 <ashu.neema28@gmail.com>
2022-06-06 07:06:09 -0600

    Updating the edit post feature

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
+  const { state , editBlogPost} = useContext(Context);
+
+  const blogPost = state.find(
+    blogPost => blogPost.id === navigation.getParam('id')
+  );
+
   return (
-    <View>
-      <Text>Edit Screen - {navigation.getParam('id')}</Text>
-    </View>
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+         editBlogPost(navigation.getParam('id'), title,content, () => navigation.pop())
+      }}
+    />
   );
 };
 
